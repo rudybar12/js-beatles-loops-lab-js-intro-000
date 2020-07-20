3d7e06918effc5c8e2639b220a2798e5374b5a4f
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-20 01:27:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var p = [];
+  for (var i = 0; i < musicians.lenght; i++){
+    p.push(musicians[i] + 'plays' + instruments[i])
+  }
+  return p;
+}
